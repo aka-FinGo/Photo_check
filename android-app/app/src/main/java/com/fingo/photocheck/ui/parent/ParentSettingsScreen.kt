5f09016dab37c053dcd5f59b855e943a46e7480b
aka-FinGo <akafingo@gmail.com>
2026-09-02 00:28:34 +0500

    Fix missing Compose UI imports in ParentSettingsScreen

--- android-app/app/src/main/java/com/fingo/photocheck/ui/parent/ParentSettingsScreen.kt
+++ android-app/app/src/main/java/com/fingo/photocheck/ui/parent/ParentSettingsScreen.kt
@@ -17,6 +17,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fingo.photocheck.model.MediaItem
 import com.fingo.photocheck.update.UpdateDialog
 import com.fingo.photocheck.update.UpdateInfo
